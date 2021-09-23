2bc143407531a728c739b7c5ec49ae1a96737b36
lamhoangvu217 <lamhoangvu217@gmail.com>
2021-09-23 09:15:04 +0700

    xoa link

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -4,7 +4,7 @@ const headerNavLinks = [
   { href: '/projects', title: 'Dịch vụ' },
   { href: '/about', title: 'Mình là ai?' },
   { href: 'https://tally.so/r/3xQO9w', title: 'Gửi câu hỏi' },
-  { href: 'https://unghotoi.com/lamvuhoang', title: 'Donate' },
+  // { href: 'https://unghotoi.com/lamvuhoang', title: 'Donate' },
 ]
 
 export default headerNavLinks
