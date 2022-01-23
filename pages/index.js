f35a1c46b14dee2cafd03bed6b29a4300017844b
lamhoangvu217 <lamhoangvu217@gmail.com>
2022-01-23 10:26:54 +0700

    add sticker

--- pages/index.js
+++ pages/index.js
@@ -18,42 +18,11 @@ export default function Home({ posts }) {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          {/* <h1 className="my-28 sm:my-10 text-center select-none text-8xl sm:text-8.5xl leading-none tracking-tightest font-extrabold">
-            <span
-              data-content="Blog."
-              className="relative block before:content-[attr(data-content)] dark:before:content-[attr(data-content)] before:w-full before:block before:absolute before:top-0 before:bottom-0 before:left-0 before:px-2 before:text-center before:text-white dark:before:text-transparent before:animate-gradient-background-1"
-            >
-              <span className="px-2 text-transparent bg-clip-text bg-gradient-to-br from-gradient-1-start to-gradient-1-end animate-gradient-foreground-1">
-                Blog.
-              </span>
-            </span>
-            <span
-              data-content="Showcase."
-              className="relative block before:content-[attr(data-content)] dark:before:content-[attr(data-content)] before:w-full before:block before:absolute before:top-0 before:bottom-0 before:left-0 before:px-2 before:text-center before:text-white dark:before:text-transparent before:animate-gradient-background-2"
-            >
-              <span className="px-2 text-transparent bg-clip-text bg-gradient-to-br from-gradient-2-start to-gradient-2-end animate-gradient-foreground-2">
-                Showcase.
-              </span>
-            </span>
-            <span
-              data-content="Portfolio."
-              className="relative block before:content-[attr(data-content)] dark:before:content-[attr(data-content)] before:w-full before:block before:absolute before:top-0 before:bottom-0 before:left-0 before:px-2 before:text-center before:text-white dark:before:text-transparent before:animate-gradient-background-3"
-            >
-              <span className="px-2 text-transparent bg-clip-text bg-gradient-to-br from-gradient-3-start to-gradient-3-end animate-gradient-foreground-3">
-                Portfolio.
-              </span>
-            </span>
-            
-          </h1> */}
-          {/* <h1 className="text-4xl font-bold leading-7 text-black dark:text-white">Dev Vũ Wonderland</h1> */}
-          {/* <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p> */}
           <div className="flex">
             <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
               Xin chào
             </h1>
-            <img src="/images/bongo.gif" className="w-20 ml-3" alt="" />
+            <img src="/images/bongo.gif" className="w-20 h-14 ml-3" alt="" />
           </div>
 
           <p className="text-lg leading-7 text-gray-600 dark:text-gray-400">
@@ -78,7 +47,10 @@ export default function Home({ posts }) {
               Blog này là nơi note lại những kiến thức mà mình học được và những điều hay ho mình
               trải nghiệm khi đi làm!
             </p>
-            <p className="my-4">Hope u enjoy ur time here sir</p>
+            <div className="flex">
+              <p className="my-4">Hope u enjoy ur time here sir</p>
+              <img src="/images/bugcat.gif" className="ml-2 w-16 h-14" alt="" />
+            </div>
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
